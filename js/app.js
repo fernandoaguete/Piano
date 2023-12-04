f6360b4efc9ae841d8481f326744d075c9eed528
FernandoAguete <fernandoaguete@hotmail.com.ar>
2023-12-04 06:58:56 -0300

    se agregaron teclas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 window.onload = () => {
     var keys = document.querySelectorAll('.key');
+    var notesArray = ['Do', 'Re', 'Mi', 'Fa', 'Sol', 'La', 'Si'];
+
+    document.onkeydown = (key) => {
+        if(key.key <= 7 && key.key >= 1) {
+            let pressedKey = notesArray[key.key - 1];
+            let div = Array.from(keys)[key.key-1];
+            div.classList.add('pressed');
+        }
+    }
+
+    document.onkeyup = () => {
+        keys.forEach(function(key) {
+            key.classList.remove('pressed');
+        })
+    }
     
     keys.forEach(function(key) {
         key.addEventListener('click', function() {
